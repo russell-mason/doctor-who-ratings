ba8dbd9f683372b0db8330a0dcc9dd8cd7c2a441
Russell Mason <public@russellmason.com>
2024-06-23 18:46:13 +0100

    Extract components
    - Extracted home page chart list navigation
    - Extracted chart page top navigation bar
    - Extracted chart tooltip content
    - Moved chart options to defaults
    - Added default fallback font

--- DoctorWhoRatings.WebClient/GlobalUsings.cs
+++ DoctorWhoRatings.WebClient/GlobalUsings.cs
@@ -1,4 +1,6 @@
+global using ApexCharts;
 global using DoctorWhoRatings.Data;
 global using DoctorWhoRatings.Data.Charting.AllEpisodes;
+global using DoctorWhoRatings.Data.Models;
 global using DoctorWhoRatings.WebClient.Components;
 global using DoctorWhoRatings.WebClient.Extensions;
