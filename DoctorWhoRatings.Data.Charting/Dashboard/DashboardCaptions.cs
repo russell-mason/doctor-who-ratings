9fc08106ab8e8ecd83e01873fb2dd84c755df0a1
Russell Mason <public@russellmason.com>
2024-07-16 15:59:34 +0100

    Fix typos

--- DoctorWhoRatings.Data.Charting/Dashboard/DashboardCaptions.cs
+++ DoctorWhoRatings.Data.Charting/Dashboard/DashboardCaptions.cs
@@ -4,5 +4,5 @@ public static class DashboardCaptions
 {
     public const string Title = "Dashboard";
 
-    public const string Description = "At-a-glace overview of the Doctor Who TV show.";
+    public const string Description = "At-a-glance overview of the Doctor Who TV show.";
 }
